61ca9ac716e5ec57aee286c4f21ad9d62eec36e3
AlexMIPT <alex00977010@mail.ru>
2014-03-29 18:55:23 +0300

    Update task_1.s

--- hw-1/task_1.s
+++ hw-1/task_1.s
@@ -1,132 +1,116 @@
-.section	.rodata
-.LC0:
-	.string	"%i"
-.LC1:
-	.string	"0%i"
+.data
+int:
+	.space	4
+int_2:
+	.space 4
+str_hello:
+	.string "\nEnter number from -1073741823 to 1073741823: "
+fmt_int:
+	.string	"%d"
+fmt_sgn:
+	.string "%d|"
+fmt_endl:
+	.string "\n\n"
+str_32:
+	.string "Signed integer value (32 bit) holds it like: "
+
 	.text
-.globl main
-	.type	main, @function
+	.globl	main
 main:
-.Label_0:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$32, %rsp
-	movl	$.LC0, %eax
-	leaq	-24(%rbp), %rdx
-	movq	%rdx, %rsi
-	movq	%rax, %rdi
-	movl	$0, %eax
-	call	__isoc99_scanf
-	movl	-24(%rbp), %eax
-	testl	%eax, %eax
-	js	.L2
-	movl	$0, -20(%rbp)
-	movl	$1, -16(%rbp)
-	jmp	.L3
-.Label_4:
-	movl	-24(%rbp), %eax
-	movl	%eax, %edx
-	sarl	$31, %edx
-	shrl	$31, %edx
-	addl	%edx, %eax
-	andl	$1, %eax
-	subl	%edx, %eax
-	imull	-16(%rbp), %eax
-	addl	%eax, -20(%rbp)
-	movl	-16(%rbp), %edx
-	movl	%edx, %eax
-	sall	$2, %eax
-	addl	%edx, %eax
-	addl	%eax, %eax
-	movl	%eax, -16(%rbp)
-	movl	-24(%rbp), %eax
-	movl	%eax, %edx
-	shrl	$31, %edx
-	leal	(%rdx,%rax), %eax
-	sarl	%eax
-	movl	%eax, -24(%rbp)
-.Label_3:
-	movl	-24(%rbp), %eax
-	testl	%eax, %eax
-	jne	.L4
-	movl	$.LC1, %eax
-	movl	-20(%rbp), %edx
-	movl	%edx, %esi
-	movq	%rax, %rdi
-	movl	$0, %eax
+	pushl	%ebp	
+	movl	%esp, 	%ebp	
+
+	pushl	$int		
+	pushl	$fmt_int	
+	call	scanf
+	addl	$8, 	%esp
+
+	movl	int,	%eax	
+	movl	%eax,	int_2
+
+	cmpl	$0,	int
+	jnl	plus
+
+	movl	$2147483648,	%ebx
+	addl	%eax,	%ebx
+	movl	%ebx,	%eax
+
+	movl	%eax,	int
+
+plus:
+
+	movl	$31,	%ebx
+fill:
+	cmpl	$0,	%ebx
+	jng	pre
+
+	pushl	$0
+	decl	%ebx
+
+	jmp	fill
+
+pre:
+	movl	int,	%eax	
+	movl	$0,	%edx	#edx = 0;
+	movl	$0,	%ebx	#ebx is counter
+	movl	$2,	%ecx	#ecx = 2;
+
+start:
+	cmpl	$0,	%eax	#(eax==0)?
+	jng	poststart
+
+	movl	$0,	%edx	
+	divl	%ecx		
+	pushl	%edx
+
+	incl	%ebx		
+	jmp	start	
+
+poststart:
+	cmpl	$32,	%ebx	
+	jnl	preinermediate
+
+	pushl	$0
+	cmp	$0,	int_2
+	jnl	zero
+	addl	$4,	%esp
+	pushl	$1	
+	zero:	
+	incl	%ebx
+
+	jmp	poststart
+
+preinermediate:
+	pushl	$fmt_sgn
+	call printf
+	addl	$8,	%esp
+	decl	%ebx
+
+intermediate:
+	cmpl	$0,	%ebx	
+	jng	end
+
+	pushl	$fmt_int		
+	call printf
+	addl	$8,	%esp
+
+	decl	%ebx		
+	jmp	intermediate
+end:	
+
+	cmpl	$0,	int
+	jne	except_zero
+	pushl	$0
+	pushl	$fmt_int
 	call	printf
-	jmp	.L5
-.Label_2:
-	movl	-24(%rbp), %eax
-	negl	%eax
-	movl	%eax, -24(%rbp)
-	movl	$0, -12(%rbp)
-	movl	$1, -8(%rbp)
-	jmp	.L6
-.Label_7:
-	movl	-24(%rbp), %eax
-	movl	%eax, %edx
-	sarl	$31, %edx
-	shrl	$31, %edx
-	addl	%edx, %eax
-	andl	$1, %eax
-	subl	%edx, %eax
-	imull	-8(%rbp), %eax
-	addl	%eax, -12(%rbp)
-	movl	-8(%rbp), %edx
-	movl	%edx, %eax
-	sall	$2, %eax
-	addl	%edx, %eax
-	addl	%eax, %eax
-	movl	%eax, -8(%rbp)
-	movl	-24(%rbp), %eax
-	movl	%eax, %edx
-	shrl	$31, %edx
-	leal	(%rdx,%rax), %eax
-	sarl	%eax
-	movl	%eax, -24(%rbp)
-.Label_6:
-	movl	-24(%rbp), %eax
-	testl	%eax, %eax
-	jne	.L7
-	movl	$0, -4(%rbp)
-	jmp	.L8
-.Label_9:
-	movl	-8(%rbp), %eax
-	addl	%eax, -4(%rbp)
-	movl	-8(%rbp), %ecx
-	movl	$1717986919, %edx
-	movl	%ecx, %eax
-	imull	%edx
-	sarl	$2, %edx
-	movl	%ecx, %eax
-	sarl	$31, %eax
-	movl	%edx, %ecx
-	subl	%eax, %ecx
-	movl	%ecx, %eax
-	movl	%eax, -8(%rbp)
-.Label_8:
-	cmpl	$0, -8(%rbp)
-	jne	.L9
-	movl	-4(%rbp), %eax
-	subl	%eax, -12(%rbp)
-	negl	-12(%rbp)
-	addl	$1, -12(%rbp)
-	movl	$.LC0, %eax
-	movl	-12(%rbp), %edx
-	movl	%edx, %esi
-	movq	%rax, %rdi
-	movl	$0, %eax
-	call	printf
-.Label_5:
-	movl	$0, %eax
-	leave
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
+	addl	$8,	%esp
+except_zero:
+
+	pushl	$fmt_endl	
+	call printf
+	addl	$4,	%esp
+
+	movl	%ebp,	%esp
+	popl	%ebp
+	movl	$0,	%eax
+	ret 
