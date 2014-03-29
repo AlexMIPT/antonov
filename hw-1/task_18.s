1e66f10c43420b6c84ed4730a39629de2de25273
AlexMIPT <alex00977010@mail.ru>
2014-03-29 22:47:19 +0300

    Create task_18.s

---
+++ hw-1/task_18.s
@@ -0,0 +1,38 @@
+  .data
+
+
+scan:
+        .string "%d"
+
+print:
+        .string "%d\n"
+
+value:
+        .long 0x00000000
+
+.text
+.globl main
+
+main:
+        pushl %ebp
+        movl %esp, %ebp 
+
+        pushl $value
+        pushl $scan
+        call scanf
+        addl $8, %esp
+
+        movl value, %ecx
+label:                     
+        movl %ecx, %eax    
+        pushl %eax         
+        pushl $print
+        call printf        
+        addl $4, %esp
+        popl %eax
+        movl %eax, %ecx
+        loop label
+
+        movl %ebp, %esp  
+        popl %ebp
+        ret
