32ea15339aa87ee2ba4a84c3dcaf9bd1e20428b6
AlexMIPT <alex00977010@mail.ru>
2014-03-29 20:18:49 +0300

    Create task_5.s

---
+++ hw-1/task_5.s
@@ -0,0 +1,74 @@
+	.data
+int:
+	.space	4
+fmt_float:
+	.string "%f"
+fmt_int:
+	.string	"%d"
+fmt_intn:
+	.string "%d\n"
+fmt_endl:
+	.string "\n"
+
+	.text
+	.globl	main
+	.globl  show
+main:
+	pushl	%ebp		
+	movl	%esp, 	%ebp	
+
+	pushl	$int		
+	pushl	$fmt_float	
+	call	scanf
+	addl	$8, 	%esp
+
+	pushl	int
+	pushl	$32
+	call	show
+	addl	$8,	%esp
+
+	pushl	$fmt_endl	
+	call printf
+	addl	$4,	%esp
+
+	movl	%ebp,	%esp	
+	popl	%ebp		
+	movl	$0,	%eax
+	ret 
+
+show:
+	pushl	%ebx
+	pushl	%ecx
+	pushl	%edx	
+	pushl	%ebp		
+	movl	%esp, 	%ebp	
+
+	movl 20(%ebp),	%ecx
+	movl 24(%ebp),	%ebx
+
+	movl 20(%ebp),	%ecx
+l_r:
+	movl	$1,	%edx
+	rorl	$1,	%ebx
+	jc	one
+	movl	$0,	%edx
+     	one:	
+	pushl	%edx
+	loop l_r	
+
+	movl 20(%ebp),	%ecx
+l_p:
+	movl	%ecx,	%ebx
+	pushl $fmt_int
+	call printf
+	addl	$8,	%esp	
+	movl	%ebx,	%ecx
+	loop l_p
+
+	movl	%ebp,	%esp	
+	popl	%ebp		
+	popl	%edx
+	popl	%ecx
+	popl	%ebx
+	ret 
+
