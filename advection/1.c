24ba544db84598f0c6fa13affcd3ddb5a94a268f
AlexMIPT <alex00977010@mail.ru>
2014-04-21 18:37:43 +0400

    Update 1.c
    Still does not work

--- advection/1.c
+++ advection/1.c
@@ -1,1 +1,45 @@
- 
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+save_id(double *x, double *y, int N);
+
+int main() {
+	int N, M;
+	int C=1; double k=0.6;
+	int a;
+	double h, t;
+	scanf("%i", &N);
+
+	h=((2*C)/(N-1));
+
+printf("%s\n", h);
+	t=k*h/C;
+	
+
+	int i;
+	float q[N];
+	for (i=1; i<=M; i++) {
+		if (i>N) {
+			a=i;
+			while (a>N) a=a-N;
+			q[a]=(C*t+1-h*i)/(h*(i-1)-1) + 1; 
+			
+		}
+		else
+			 {
+			q[i]=(C*t+1-h*i)/(h*(i-1)-1) + 1;
+	
+		}
+
+	}
+
+	return 0;
+}
+
+
+save_id(double *x, double *y, int N) {                                                                                        
+                                                                          
+                                                                
+}
