1bfab12a8db979e403cfd176eec980cc258e7cb1
AlexMIPT <alex00977010@mail.ru>
2014-05-05 18:45:33 +0400

    Update task_1.s

--- hw-2/task_1.s
+++ hw-2/task_1.s
@@ -1,32 +1,87 @@
 	.data
-str_a:	.string "%d"
-str_b:	.string "%d\n"
+del:
+pushl %ebp
+        movl %esp, %ebp
 
-		.text
-	.globl main
-	.type	main, @function
+        pushl %ebx
+        movl 8(%ebp),%eax
+        movl 12(%ebp),%ebx
+movl %eax,%edx
+sarl $31, %edx
+        idivl %ebx
+        popl %ebx
+
+
+        movl %ebp, %esp
+        popl %ebp
+        ret
+int_1:
+        .space 4
+int_2:
+        .space 4
+out_string:
+        .string "%d\n"
+scan:
+        .string "%d %d"
+string_warn: .string "Division with remainder\n"
+.text
+.globl del, main
 main:
-	pushl	%ebp
-	movl	%esp, %ebp
+        pushl %ebp	
+        movl %esp, %ebp
 
+pushl $int_2
+        pushl $int_1
+        pushl $scan
+        call scanf	
+        addl $8, %esp
 
-	pushl	%esp
+        
+pushl int_2
+pushl int_1
+call del
+addl $8, %esp
+
+pushl %eax
+pushl $out_string
+call printf
+addl $8, %esp
+
+        
+	pushl int_2 
+	pushl int_1
+	call del     
+	addl $8, %esp
+	pushl %eax
+
+        mull int_2
+	movl %eax, %ebx
+        
+       
+        pushl $int_1
+	movl int_1, %eax
 	
-	pushl	$str_a
-	call scanf
-	movl  8(%esp),	%eax	
-	addl	$8,	%esp
 
-	movl	%eax, %edx
-	shrl	$31, %edx	 
-	leal	(%edx,%eax), %eax
-	sarl	%eax	 
 
-	pushl	%eax
-	pushl	$str_b
+	cmpl %ebx, %eax
+	
+	je loop_end
+
+	jmp loop_start
+
+loop_start:
+
+pushl %eax
+	pushl $string_warn
 	call printf
-	addl	$8,	%esp
+	addl $8, %esp
 
-	movl	$0, %eax
-	leave
+loop_end:
+
+
+        movl %ebp, %esp	
+        popl %ebp
+      
+        movl $0, %eax
+
 ret
